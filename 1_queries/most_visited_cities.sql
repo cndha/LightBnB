b35bb1f4c771a1c16977507ea17a1ff063880ca0
Cindy H <cindyhaa@gmail.com>
2021-10-14 00:06:40 +0000

    queries

---
+++ 1_queries/most_visited_cities.sql
@@ -0,0 +1,6 @@
+SELECT properties.city, Count(reservations) AS total_reservations
+FROM properties
+JOIN reservations ON property_id = properties.id
+GROUP BY city 
+ORDER BY total_reservations DESC;
+
